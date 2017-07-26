f0f785fe71290d38fa8f210c22ac38892f8c8748
yureev <y.beletskiy@gmail.com>
2017-07-26 17:52:28 +0300

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from "@angular/router";
+import {FormsModule, NgForm} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { RoleComponent } from './role/role.component';
+
+// определение маршрутов
+const appRoutes: Routes = [
+  { path: '', component: AuthComponent},
+  { path: 'auth', component: AuthComponent},
+  { path: 'role', component: RoleComponent},
+  { path: '**', redirectTo: '/'}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, RouterModule.forRoot(appRoutes), FormsModule],
+  declarations: [ AppComponent, AuthComponent, RoleComponent],
+  bootstrap:    [ AppComponent ],
+  providers: []
 })
 export class AppModule { }
