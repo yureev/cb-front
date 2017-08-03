d84765dfc221710564b33d52f8185416b69fb898
yureev <y.beletskiy@gmail.com>
2017-08-03 16:06:57 +0300

    design aplication

--- src/app/role/role.component.ts
+++ src/app/role/role.component.ts
@@ -1,25 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm} from '@angular/forms';
 import {AuthService} from '../auth/auth.service';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+const getGraphql = gql`
+ query getGraphql($idCode: String) {
+    customer(idCode: $idCode) {
+    id
+    name
+  }
+  }
+`;
 
 @Component({
   selector: 'app-role',
   templateUrl: './role.component.html',
   styleUrls: ['./role.component.sass']
 })
-export class RoleComponent  {
+export class RoleComponent implements OnInit {
   name = localStorage.getItem('name');
   rolename = localStorage.getItem('rolename');
+  responce: any;
+  searchClient: boolean;
 
-  constructor(private authService: AuthService) {}
-  logout() {
-    this.authService.logout();
+  constructor(private authService: AuthService, private apollo: Apollo) {}
+    logout() {
+      this.authService.logout();
   }
 
-  // ngOnInit() {
-  //   name = localStorage.getItem('name');
-  //   rolename = localStorage.getItem('rolename');
-  //
-  //   console.log('fdfdf', name, rolename);
-  // }
+  onSubmit( form: NgForm) {
+
+    // let variab = "2217606611";
+    let variab2 = form.value.ipnNumber;
+
+    console.log(variab2);
+
+    this.apollo.query({
+      query: getGraphql,
+      variables: {
+        idCode: variab2
+      }
+    }).subscribe((res) => {
+      this.responce = res;
+      console.log('name', this.responce.data.customer.name);
+    });
+
+    setTimeout(() => {
+      this.apollo.query({
+        query: getGraphql,
+        variables: {
+          idCode: variab2
+        }
+      }).subscribe((res) => {
+
+        console.log(res);
+        this.responce = res;
+        console.log('name', this.responce.data.customer.name);
+      });
+    }, 2000);
+  }
+
+  ngOnInit() {
+    this.searchClient = false;
+  }
 
 }
